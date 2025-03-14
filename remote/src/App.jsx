0f7931a8c9f2d0de53e26ce827dfd59ec30949b4
nzkks <kkshanthosh@gmail.com>
2025-03-15 07:36:47 +1300

    Remote: replace the useState hook in the App component too for the normal html button

--- remote/src/App.jsx
+++ remote/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
+import useCount from './store';
 import './App.css';
 import RemoteButton from './components/RemoteButton';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useCount();
 
   return (
     <>
       <h1>Remote React application</h1>
       <RemoteButton />
       <div className="card">
+        {/* Since both RemoteButton component and the normal html button below uses the same store, 
+        clicking either one updates the store and gets the latest value to show in both buttons */}
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
       </div>
     </>
