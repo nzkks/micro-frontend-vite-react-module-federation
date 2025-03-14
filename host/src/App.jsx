d4149533abd5121fa2aa7705dc5ed70c91a8f435
nzkks <kkshanthosh@gmail.com>
2025-03-14 21:34:12 +1300

    Host: import and use RemoteButton from remoteApp

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 
+import RemoteButton from 'remoteApp/RemoteButton';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Host React application</h1>
+      <RemoteButton />
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
       </div>
