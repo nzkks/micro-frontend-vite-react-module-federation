fd432b334f1134d04aca137da7abaa01627005df
nzkks <kkshanthosh@gmail.com>
2025-03-14 21:23:14 +1300

    Remote: create and add RemoteButton component in App.jsx

--- remote/src/App.jsx
+++ remote/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import RemoteButton from './components/RemoteButton';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Remote React application</h1>
+      <RemoteButton />
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
       </div>
